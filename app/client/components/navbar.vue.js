04d53196af13ddd7b2cb58ece0079a486fa9e1e2
Pedro Borges <pedro.hov.borges@gmail.com>
2021-07-16 01:11:48 -0300

    update navbar

--- app/client/components/navbar.vue.js
+++ app/client/components/navbar.vue.js
@@ -1,22 +1,47 @@
 const navbar = Vue.component("Navbar", {
     template: /* html */
         `
-        <v-container>
-            <v-app-bar app color="primary" dark dense>
-                <v-toolbar-title>
-                    <v-btn text :to="{ name: 'Home' }">App</v-btn>
-                </v-toolbar-title>
-                <v-spacer></v-spacer>
+        <v-app-bar color="primary" dark dense>
+            <v-toolbar-title>
+                <v-btn text :to="{ name: 'Home' }">Home</v-btn>
+            </v-toolbar-title>
+            
+            <template v-if="isAuthenticated">
+                <v-btn color="primary" :to="{ name: 'Favorites' }" class="ml-2">Favoritos</v-btn>
+                <v-btn color="primary" :to="{ name: 'Wishlist' }" class="ml-2">Interesses</v-btn>
+            </template>
+            
+            <v-spacer></v-spacer>
+            
+            <v-menu offset-y v-if="isAuthenticated && isAdmin">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
+                    >
+                        Admin
+                    </v-btn>
+                </template>
                 
-                <template v-if="!isAuthenticated && $route.path != '/login'">
-                    <v-btn :to="{ name: 'Login' }">Login</v-btn>
-                </template>
-                <template v-else-if="isAuthenticated">
-                    <span class="mr-5">Logged in as {{ $store.getters.user.username }}</span>
-                    <v-btn @click="logout">Logout</v-btn>
-                </template>
-            </v-app-bar>
-        </v-container>
+                <v-list>
+                    <v-list-item>
+                        <v-btn dark :to="{ name: 'AddGame' }">Jogos</v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn dark :to="{ name: 'Platforms' }">Plataformas</v-btn>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+            
+            <template v-if="!isAuthenticated && $route.path != '/login'">
+                <v-btn :to="{ name: 'Login' }">Login</v-btn>
+            </template>
+            <template v-else-if="isAuthenticated">
+                <span class="mx-5">Logged in as {{ $store.getters.user.username }}</span>
+                <v-btn @click="logout">Logout</v-btn>
+            </template>
+        </v-app-bar>
     `,
 
     props: [],
@@ -31,6 +56,9 @@ const navbar = Vue.component("Navbar", {
     computed: {
         isAuthenticated() {
             return this.$store.getters.isAuthenticated;
+        },
+        isAdmin() {
+            return this.$store.getters.isAdmin;
         }
     },
 
