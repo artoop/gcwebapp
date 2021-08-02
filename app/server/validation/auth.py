e6d9b24dc8229cf044a0f01bcf0ba872c12f2978
Artur Braga <61604082+artoop@users.noreply.github.com>
2021-08-02 11:03:07 -0300

    authValidation

--- app/server/validation/auth.py
+++ app/server/validation/auth.py
@@ -9,6 +9,62 @@ from app.server import bp
 
 import string
 
+
+@json_request
+def authLogin():
+    data = request.json
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if not is_user_request_valid(data, False):
+        return jsonify({
+            "success": False,
+            "message": "Missing username or password"
+        }), 400
+
+    if not validate_username(data.get("username", "")):
+        return jsonify({
+            "success": False,
+            "message": "Bad username"
+        }), 400
+
+    db = get_db()
+
+    user = db.users.find_one({
+        "username": username
+    })
+
+    if user is None:
+        return jsonify({
+            "success": False,
+            "message": "User not found"
+        }), 404
+
+    # check for password match
+    if not check_password_hash(user["password"], password):
+        return jsonify({
+            "success": False,
+            "message": "Username or password don't match"
+        }), 400
+
+    # clear and set new session
+    session.clear()
+    session["user_id"] = str(user["_id"])
+
+    return jsonify({
+        "success": True,
+        "message": "Logged in",
+        "data": {
+            "user": {
+                "username": user["username"],
+                "groups": user.get("groups", ["user"])
+            }
+        }
+    }), 200
+
+
+@json_request
 def authRegister():
     data = request.json
 
@@ -46,8 +102,6 @@ def authRegister():
         "success": True,
         "message": "Registered successfully"
     }), 201
-
-
 
 def authLogout():
     session.clear()
